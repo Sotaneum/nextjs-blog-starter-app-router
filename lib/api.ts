0e810c33671d4eae0e520cce0e41838de9dd2e0e
Sotaneum <gnyontu39@gmail.com>
2023-07-30 13:53:03 +0900

    export mode 설정 적용

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { join } from 'path';
+import {join} from 'path';
 import matter from 'gray-matter';
 
 import PostType from '@/interfaces/post';
---
+++ next.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
